957b5702811ea5dd0c868b8b454fbf87651e88d3
Vincent Vole <volevincent@gmail.com>
2017-08-09 15:20:25 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  now = Date.now();
+  diff = 0;
+  active = '';
+
+  timeTick(){
+  	setTimeout(() => {
+  		this.now = Date.now();
+  		this.now+=this.diff;
+  		this.timeTick();
+  	}, 100);
+  }
+
+  showTime(zone){
+  	if(zone == 'pst'){
+  		this.diff = 0;
+  	}
+  	else if(zone == 'mst'){
+  		this.diff = 3600000;
+  	}
+  	else if(zone == 'cst'){
+  		this.diff = 7200000;
+  	}
+  	else if(zone == 'est'){
+  		this.diff = 10800000;
+  	}
+  		this.active = zone;
+  }
+
+  hideTime(){
+  	this.active = '';
+  }
+
+  constructor(){
+  	this.timeTick();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // <-- Include module in our AppModules
+	HttpModule // <-- Include module in our AppModules
   ],
   providers: [],
   bootstrap: [AppComponent]
